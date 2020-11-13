b001748bdc38e1fe3b2d4d42ce26a14098265352
CaioSilva584 <69801765+CaioSilva584@users.noreply.github.com>
2020-11-13 19:38:07 -0300

    Update checkbox.js

--- Atividade 11/checkbox.js
+++ Atividade 11/checkbox.js
@@ -8,5 +8,7 @@ function alterarShift(){
         document.getElementById("idNome").value = document.getElementById("idNome").value.toUpperCase();
     }else if(chkMin.checked){
         document.getElementById("idNome").value = document.getElementById("idNome").value.toLowerCase();
+    }else{
+        alert("Selecione uma das opções");
     }
 }
